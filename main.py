c036b6484c58de054eebc5c2a39531fa99e222c7
jarickzales <zalesjarix@gmail.com>
2025-09-04 22:58:33 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+from pyscript import display
+
+restaurant_name = "Portiento's" 
+#this cafe idea was reused from our entrep project during Grade 8 TLE, also this is a string
+
+owner_name = "Riso Salapunen" 
+#this is also a string
+
+year_established = 2025 
+#this is an integer
+
+popular_item_price = "Php 149"
+#despite having a number, this is a string because of thee quotation marks
+
+has_delivery = True 
+#this is a boolean 
+
+product_names = ['Pandesal', 'Spanish Latte', 'Pianono Rolls']
+#this data type is a list
+
+business_hours = "24/7"
+#again, despite having numbers, it is a string.
+
+menu_prices = {'Pandesal':'Php 5/pc', 'Pianono Rolls':'Php 20/pc', 'Pan de Regla':'Php 8/pc', 
+'Pan de Coco':'Php 15/pc', 'Spanish Latte':'Php 145', 'Frappuccino':'Php 180', 'Cappuccino':'170 Php'}
+
+'''
+I used a dictionary as the guidelines mentioned "item-price pairs" and the first type of data that came to mind was
+the dictionary data type
+'''
+
+common_allergens = ['milk', 'eggs', 'wheat']
+#this is also a list
+
+tax_rate = 0.07
+#this has a decimal point which makes it a float
+
+#display(restaurant_name, owner_name, year_established, popular_item_price, has_delivery, product_names, business_hours, menu_prices, common_allergens, tax_rate)
